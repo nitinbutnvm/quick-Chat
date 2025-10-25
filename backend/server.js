2b87fe0b31318bdcbfb1776fc42c937249f9579f
nitinbutnvm <heynittinnn@gmail.com>
2025-10-25 21:10:00 +0530

    Initialize server setup with Express, middleware, and database connection

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import http from "http"
+import { connectDB } from "./lib/db.js"
+
+const app = express()
+const server = http.createServer(app)
+
+// middleware
+app.use(cors())
+app.use(express.json({limit:"4mb"})) 
+
+app.use("api/status", (req, res)=>res.send("Server is running"))
+
+await connectDB();
+
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => {
+    console.log(`Server is running  ${PORT}`)
+})
