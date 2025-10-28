aaefbef8aa3fc5472c99b3cf9d85e3d28379a9fa
nitinbutnvm <heynittinnn@gmail.com>
2025-10-28 18:52:52 +0530

    Implement getUsersForSidebar function to retrieve users and count unseen messages

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,7 +1,29 @@
-
+import User from "../models/User.js";
 
 // get all user except lgin user
-export const getUserForSidebar = async()=>{
+export const getUsersForSidebar = async () => {
+  try {
+    const userId = requestAnimationFrame.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: userId } }).select(
+      "-password"
+    );
 
-}
-
+    // count no of unseen message
+    const unseenMessages = {};
+    const promises = filteredUsers.map(async (user) => {
+      const messages = await Message.find({
+        senderId: user._id,
+        receiverId: userId,
+        seen: false,
+      });
+      if (messages.length > 0) {
+        unseenMessages[user._id] = messages.length;
+      }
+    });
+    await Promise.all(promises);
+    return res.json({ success:true, users: filteredUsers, unseenMessages });
+  } catch (error) {
+    console.log(error.messages);
+    return res.json({ success:false, message:error.message });
+  }
+};
