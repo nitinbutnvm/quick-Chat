199d028670bcb4894c86cf8631267612ae5e37fa
nitinbutnvm <heynittinnn@gmail.com>
2025-11-01 11:09:02 +0530

    Add ChatContext for managing chat messages and users

---
+++ frontend/context/ChatContext.jsx
@@ -0,0 +1,51 @@
+import { createContext, useContext, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import toast from "react-hot-toast";
+
+
+
+export const ChatContext =  createContext();
+
+export const ChatProvider = ({children}) => {
+
+    const [messages, setMessages] = useState([])
+    const [users, setUsers] = useState([])
+    const [selectedUser, setSelectedUser] = useState(null)
+    const [unseenMessages, setUnseenMessages] = useState([])
+
+    const {socket, axios} = useContext(AuthContext)
+
+    const getUsers = async () => {
+        try {
+            const {data} = await axios.get("/api/messages/users")
+            if (data.success) {
+                setUsers(data.users)
+                setUnseenMessages(data.unseenMessages)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+
+    const getMessages = async (userId) => {
+        try {
+            const {data} = await axios.get(`/api/messages/${userId}`)
+            if (data.success) {
+                setMessages(data.messages)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const  value = {
+
+    }
+
+    return (
+        <ChatContext.Provider value={value}>
+            {children}
+        </ChatContext.Provider>
+    )
+} 
