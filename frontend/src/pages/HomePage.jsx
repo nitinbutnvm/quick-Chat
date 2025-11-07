ce3bfcd5d62ca37dccda04cefeb88beefc2fea72
nitinbutnvm <heynittinnn@gmail.com>
2025-11-07 18:07:13 +0530

    Refactor HomePage to use ChatContext for selectedUser state management

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react"
+import { useContext } from "react"
 import ChatContainer from "../components/ChatContainer"
 import RightSidebar from "../components/RightSidebar"
 import Sidebar from "../components/Sidebar"
+import { ChatContext } from "../../context/ChatContext"
 
 const HomePage = () => {
-  const [selectedUser, setSelectedUser] = useState(false)
+  const {selectedUser} = useContext(ChatContext)
 
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%] ">
@@ -12,7 +13,7 @@ const HomePage = () => {
         overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr] ' : 'md:grid-cols-2 '} `} >
         <Sidebar />
         <ChatContainer  />
-        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+        <RightSidebar/>
       </div>
     </div>
   )
